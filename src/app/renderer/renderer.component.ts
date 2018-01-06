ba47995ecd175288cce868e5c7030f84a1a759b9
Manojlović Miloš <manojlovicsmilos@hotmail.com>
2018-01-06 12:48:58 +0100

    View Rotation

--- src/app/renderer/renderer.component.ts
+++ src/app/renderer/renderer.component.ts
@@ -13,9 +13,14 @@ import { Document } from "./../document";
 export class RendererComponent
 {
     @Input() Document:Document;
+    private _MousePos:any;
+    private _MouseDown:boolean;
     private _Renderer:Renderer;
     public get Renderer():Renderer { return this._Renderer; }
-    public constructor() {}
+    public constructor() 
+    {
+        this._MouseDown = false;
+    }
     public ngOnInit() : void 
     {
         if(this.Document.Data != null)
@@ -27,6 +32,33 @@ export class RendererComponent
         {
             this._Renderer = new Renderer({X:512, Y:512}, this.Document);
             this.Document.Data = this._Renderer;
+        }
+    }
+    public MouseDown(Event) : void
+    {
+        this._MouseDown = true;
+        this._MousePos = {X:Event.offsetX, Y:Event.offsetY};
+    }
+    public MouseUp(Event) : void
+    {
+        this._MouseDown = false;
+    }
+    public MouseMove(Event) : void
+    {
+        if(this._MouseDown)
+        {
+            if(this._Renderer.Models.Selected)
+            {
+                this._Renderer.Models.Selected.RotationY += (Event.offsetX - this._MousePos.X);
+            }
+            else
+            {
+                for(let i in this._Renderer.Models.Models)
+                {
+                    this._Renderer.Models.Models[i].RotationY += (Event.offsetX - this._MousePos.X);
+                }
+            }
+            this._MousePos = {X:Event.offsetX, Y:Event.offsetY};
         }
     }
 }
